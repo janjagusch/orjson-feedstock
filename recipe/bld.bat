275e61e4e531818fe63574dec0caa064b52a265d
Joshua L. Adelman <joshua.adelman@gmail.com>
2020-08-04 14:36:36 -0400

    debug windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,12 @@ FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%\rust-nightly-install"') DO s
 bash %SRC_DIR%\rust-nightly\install.sh --verbose --prefix=%pfx% --disable-ldconfig
 set PATH=%SRC_DIR%\rust-nightly-install\bin:%PATH%
 
+echo "check cargo"
+cargo --help
+
+echo "check cargo.exe"
+cargo.exe --help
+
 maturin build --no-sdist --release --strip --manylinux off
 
 %PYTHON% -m pip install . -vv
