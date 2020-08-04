4f489ce1c4000aa0802d0276f5adbc0f4cedb0b2
Joshua L. Adelman <joshua.adelman@gmail.com>
2020-08-04 13:10:17 -0400

    add windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,6 @@ fi
 ${SRC_DIR}/rust-nightly/install.sh --verbose --prefix=${SRC_DIR}/rust-nightly-install --disable-ldconfig
 export PATH=${SRC_DIR}/rust-nightly-install/bin:$PATH
 
-ls -l
-
 maturin build --no-sdist --release --strip --manylinux off
+
 "${PYTHON}" -m pip install . -vv
