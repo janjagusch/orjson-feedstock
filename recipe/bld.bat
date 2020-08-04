85d6d985fbc40dcc9745bd17629a19062967b85b
Marcel Bargull <mbargull@users.noreply.github.com>
2020-08-04 23:22:43 +0200

    Fix Windows path separator

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,6 @@
 FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%\rust-nightly-install"') DO set "pfx=%%i"
 bash %SRC_DIR%\rust-nightly\install.sh --verbose --prefix=%pfx% --disable-ldconfig
-set PATH=%SRC_DIR%\rust-nightly-install\bin:%PATH%
-
-echo "check cargo"
-cargo --help
-
-echo "check cargo.exe"
-cargo.exe --help
+set "PATH=%SRC_DIR%\rust-nightly-install\bin;%PATH%"
 
 maturin build --no-sdist --release --strip --manylinux off
 
