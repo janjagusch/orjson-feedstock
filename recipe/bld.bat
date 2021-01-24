ab0877245138865e00246fed9a0e76ce564d9859
Nehal J Wani <nehaljw.kkd1@gmail.com>
2021-01-24 11:35:17 -0500

    Specify interpreter explicitly

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,6 @@ FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%\rust-nightly-install"') DO s
 bash %SRC_DIR%\rust-nightly\install.sh --verbose --prefix=%pfx% --disable-ldconfig
 set "PATH=%SRC_DIR%\rust-nightly-install\bin;%PATH%"
 
-maturin build --no-sdist --release --strip --manylinux off
+maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON%
 
 %PYTHON% -m pip install . -vv
