75a14b4d633c9534affa573f8c470b101ddc683d
Nehal J Wani <nehaljw.kkd1@gmail.com>
2021-08-18 17:48:41 -0400

    Fix args specified to maturin

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,8 +15,8 @@ if [ "$c_compiler" = gcc ] ; then
     export CARGO_TARGET_${rust_env_arch}_LINKER=$CC
 fi
 
-declare -a _xtra_cargo_args
-_xtra_cargo_args+=(-Zfeatures=itarget)
+declare -a _xtra_maturin_args
+_xtra_maturin_args+=(--cargo-extra-args="-Zfeatures=itarget")
 
 if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
     # Install the standard host stuff for target platform
@@ -39,8 +39,8 @@ rustflags = [
 ]
 
 EOF
-  _xtra_cargo_args+=(--target=aarch64-apple-darwin)
-  _xtra_cargo_args+=(--features=aarch64_neon)
+  _xtra_maturin_args+=(--cargo-extra-args="--features=aarch64_neon")
+  _xtra_maturin_args+=(--target=aarch64-apple-darwin)
 
   # This variable must be set to the directory containing the target's libpython DSO
   export PYO3_CROSS_LIB_DIR=$PREFIX/lib
@@ -56,6 +56,6 @@ cd -
 
 export PATH=${SRC_DIR}/rust-nightly-install/bin:$PATH
 
-maturin build --no-sdist --release --strip --manylinux off --cargo-extra-args="${_xtra_cargo_args[@]}" --interpreter="${PYTHON}"
+maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" "${_xtra_maturin_args[@]}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/orjson*.whl --no-deps -vv
